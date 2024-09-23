2931e136faeffa97249de5843a0f0d8928ba5a58
Jakub <jm54706@outlook.com>
2024-09-23 02:57:24 +0200

    Mobile navigation - work in progress

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,13 +1,20 @@
 import './Navigation.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGear,
+	faChartLine,
+	faMoneyBillTransfer,
+	faWallet,
+	faSackDollar,
+	faCoins,
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 
 function Navigation() {
 	const [menu, setMenu] = useState(false);
 
-	let navBtn
+	let navBtn;
 
 	useEffect(() => {
 		navBtn = document.querySelector('.nav__btn');
@@ -20,7 +27,7 @@ function Navigation() {
 
 	return (
 		<>
-			<nav className='nav'>
+			{/* <nav className='nav'>
 				<div className='nav__btn-box'>
 					<span className='nav__alert'>
 						<FontAwesomeIcon icon={faBell} />
@@ -53,9 +60,53 @@ function Navigation() {
 					<li className='nav__list-element'>Budżety</li>
 					<li className='nav__list-element'>Oszczędności</li>
 				</ul>
-			</nav>
+			</nav> */}
 
-			<div className='test'></div>
+			<nav className='nav-mobile'>
+				<div className='nav-mobile__menu-container'>
+					<div className='nav-mobile__menu-box'>
+						<a href='#' className='nav-mobile__menu-link'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faChartLine} />
+							</span>
+							Panel
+						</a>
+						<a href='#' className='nav-mobile__menu-link'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faMoneyBillTransfer} />
+							</span>
+							Transakcje
+						</a>
+						<a href='#' className='nav-mobile__menu-link'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faWallet} />
+							</span>
+							Portfele
+						</a>
+						<a href='#' className='nav-mobile__menu-link'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faSackDollar} />
+							</span>
+							Budżety
+						</a>
+						<a href='#' className='nav-mobile__menu-link'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faCoins} />
+							</span>
+							Oszczędności
+						</a>
+						<a href='#' className=' nav-mobile__menu-settings'>
+							<span className='nav-mobile__menu-icon'>
+								<FontAwesomeIcon icon={faGear} />
+							</span>
+							Ustanwienia
+						</a>
+						<a href='#' className=' nav-mobile__menu-logout'>
+							Wyloguj
+						</a>
+					</div>
+				</div>
+			</nav>
 		</>
 	);
 }
