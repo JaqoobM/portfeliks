c7fdbad3c4a056984840a65ed99f52301f34d7a9
Jakub <jm54706@outlook.com>
2024-09-26 02:58:57 +0200

    Added animation to navigations

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -15,14 +15,16 @@ import { useEffect, useState } from 'react';
 
 function Navigation() {
 	const [menu, setMenu] = useState(false);
+	const [menuIsOpen, setMenuIsOpen] = useState(true);
 
 	let navBtn;
 	let navMobile;
 	let menuIconsArr;
 	let menuTextsArr;
+	let desktopBurgerBtn;
 
 	useEffect(() => {
-		navBtn = document.querySelector('.nav__btn');
+		desktopBurgerBtn = document.querySelector('.burger-btn-desktop');
 		navMobile = document.querySelector('.nav-mobile');
 		menuIconsArr = document.querySelectorAll('#menuIcon');
 		menuTextsArr = document.querySelectorAll('#menuText');
@@ -37,54 +39,69 @@ function Navigation() {
 		if (btn.includes('burger-btn-mobile')) {
 			navMobile?.classList.toggle('nav-mobile__menu-active-mobile');
 		} else {
-			menuIconsArr.forEach((icon) => {
-				icon.classList.toggle('nav-mobile__menu-inactive-desktop');
-			});
+			switch (menuIsOpen) {
+				case true:
+					menuIconsArr.forEach((icon) => {
+						icon.classList.toggle('nav-mobile__menu-inactive-desktop');
+					});
 
-			menuTextsArr.forEach((text) => {
-				text.classList.toggle('nav-mobile__menu-text-disabled')
-			})
+					menuTextsArr.forEach((text) => {
+						text.classList.toggle('nav-mobile__menu-text-disabled');
+					});
+					setMenuIsOpen(false)
+					break;
+				case false:
+					setTimeout(() => {
+						menuIconsArr.forEach((icon) => {
+							icon.classList.toggle('nav-mobile__menu-inactive-desktop');
+						});
+
+						menuTextsArr.forEach((text) => {
+							text.classList.toggle('nav-mobile__menu-text-disabled');
+						});
+					}, 100);
+					setMenuIsOpen(true)
+					break;
+
+				default:
+					break;
+			}
+
+			desktopBurgerBtn.firstChild.classList.toggle('burger-rotate');
+
+			navMobile.classList.toggle('nav-small');
 		}
 	};
 
 	return (
 		<>
-			{/* <nav className='nav'>
-				<div className='nav__btn-box'>
-					<span className='nav__alert'>
-						<FontAwesomeIcon icon={faBell} />
-					</span>
-
-					<button type='button' className='nav__btn' onClick={menuHandler}>
-						<p className='nav__name'>jm54706@outlook.com</p>
-
-						<span className='nav__settings'>
-							<FontAwesomeIcon icon={faGear} />
-						</span>
-
-						{menu && (
-							<div className='nav__menu-box'>
-								<ul className='nav__menu-list'>
-									<li className='nav__menu-element'>Ustawienia</li>
-									<div className='nav__menu-line'></div>
-									<li className='nav__menu-element'>Wyloguj</li>
-								</ul>
-
-								<div className='nav__menu-bg'></div>
-							</div>
-						)}
-					</button>
+			<button
+				onClick={(e) => {
+					const btn = e.target.closest('button').className;
+					mobileMenuHandler(btn);
+				}}
+				className='burger-btns burger-btn-mobile'>
+				<div className='burger-btns__box'>
+					<div className='burger-btns__bars burger-btn-mobile__bar1'></div>
+					<div className='burger-btns__bars burger-btn-mobile__bar2'></div>
+					<div className='burger-btns__bars burger-btn-mobile__bar3'></div>
 				</div>
-
-				<ul className='nav__list'>
-					<li className='nav__list-element'>Panel</li>
-					<li className='nav__list-element'>Transakcje</li>
-					<li className='nav__list-element'>Budżety</li>
-					<li className='nav__list-element'>Oszczędności</li>
-				</ul>
-			</nav> */}
+			</button>
 
 			<nav className='nav-mobile'>
+				<button
+					onClick={(e) => {
+						const btn = e.target.closest('button').className;
+						mobileMenuHandler(btn);
+					}}
+					className='burger-btns burger-btn-desktop'>
+					<div className='burger-btns__box'>
+						<div className='burger-btns__bars'></div>
+						<div className='burger-btns__bars'></div>
+						<div className='burger-btns__bars'></div>
+					</div>
+				</button>
+
 				<a href='#' className='nav-mobile__menu-link'>
 					<span id='menuIcon' className='nav-mobile__menu-icon'>
 						<FontAwesomeIcon icon={faChartLine} />
@@ -146,24 +163,6 @@ function Navigation() {
 					</a>
 				</div>
 			</nav>
-
-			<button
-				className='burger-btns burger-btn-mobile'
-				onClick={(e) => {
-					const btn = e.target.closest('button').className;
-					mobileMenuHandler(btn);
-				}}>
-				<FontAwesomeIcon icon={faBars} />
-			</button>
-
-			<button
-				className='burger-btns burger-btn-desktop'
-				onClick={(e) => {
-					const btn = e.target.closest('button').className;
-					mobileMenuHandler(btn);
-				}}>
-				<FontAwesomeIcon icon={faBars} />
-			</button>
 		</>
 	);
 }
