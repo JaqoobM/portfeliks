eccb18a5f097640f87ed9e6686636ab13fd7da7f
Jakub <jm54706@outlook.com>
2024-10-21 23:44:39 +0200

    new elements

--- frontend/src/components/Transactions/AddTransactionModal/AddTransactionModal.js
+++ frontend/src/components/Transactions/AddTransactionModal/AddTransactionModal.js
@@ -1,10 +1,7 @@
 import './AddTransactionModal.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {} from '@fortawesome/free-regular-svg-icons';
-import {
-	faChevronDown,
-	faCircleXmark,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function AddTransactionModal(props) {
 	return (
@@ -20,13 +17,19 @@ export default function AddTransactionModal(props) {
 				</button>
 				<span className='add-transaction__title'>Dodaj transakcję</span>
 				<div className='add-transaction__input-box'>
-					<label className='add-transaction__label'>Nazwa</label>
-					<input className='add-transaction__input'></input>
+					<label for='name' className='add-transaction__label'>
+						Nazwa
+					</label>
+					<input id='name' className='add-transaction__input'></input>
 				</div>
 				<div className='add-transaction__input-box'>
 					<span className='add-transaction__input-currency'>zł</span>
-					<label className='add-transaction__label'>Kwota</label>
-					<input className='add-transaction__input add-transaction__input-amount'></input>
+					<label for='amount' className='add-transaction__label'>
+						Kwota
+					</label>
+					<input
+						id='amount'
+						className='add-transaction__input add-transaction__input-amount'></input>
 				</div>
 				<div className='add-transaction__btns-box'>
 					<button
@@ -39,6 +42,13 @@ export default function AddTransactionModal(props) {
 						type='button'>
 						Przychód
 					</button>
+				</div>
+				<div className='add-transaction__date-box'>
+					<label className='add-transaction__date-label'>Data</label>
+					<input
+						className='add-transaction__date-input'
+						type='date'
+						value='2024-10-21'></input>
 				</div>
 				<button className='add-transaction__list-btn' type='button'>
 					Brak kategorii
@@ -54,7 +64,12 @@ export default function AddTransactionModal(props) {
 						<FontAwesomeIcon icon={faChevronDown} />
 					</span>
 				</button>
-				<button className='add-transaction__add-btn' type='button'>
+				<button
+					className='add-transaction__add-btn'
+					type='button'
+					onClick={() => {
+						props.addModalHandler('addBtn');
+					}}>
 					Dodaj
 				</button>
 			</div>
--- frontend/src/components/Transactions/Transactions.js
+++ frontend/src/components/Transactions/Transactions.js
@@ -1,4 +1,5 @@
 import './Transactions.scss';
+import React from 'react';
 import FiltersMobile from './FiltersMobile/FiltersMobile';
 import AddTransactionModal from './AddTransactionModal/AddTransactionModal';
 import Navigation from '../Navigation/Navigation';
@@ -11,6 +12,7 @@ import {
 	faGear,
 	faRightFromBracket,
 	faCartShopping,
+	faSquareUpRight,
 } from '@fortawesome/free-solid-svg-icons';
 import { useRef, useState } from 'react';
 
@@ -21,51 +23,79 @@ function Transactions() {
 			date: '10.05.2024',
 			name: 'Części do samochodu',
 			price: '49,99',
+			_id: 1,
 		},
 		{
 			date: '21.04.2024',
 			name: 'Lidl',
 			price: '43,25',
+			_id: 2,
 		},
 		{
 			date: '15.04.2024',
 			name: 'Biedronka',
 			price: '22,11',
+			_id: 3,
 		},
 		{
 			date: '15.04.2024',
 			name: 'Telefon',
 			price: '999,99',
+			_id: 4,
+		},
+		{
+			date: '15.04.2024',
+			name: 'Orange',
+			price: '35',
+			_id: 5,
 		},
 		{
 			date: '27.03.2024',
 			name: 'Zakupy',
 			price: '100',
+			_id: 6,
 		},
 		{
 			date: '14.03.2024',
 			name: 'Paliwo',
 			price: '220,77',
+			_id: 7,
 		},
 		{
 			date: '05.03.2024',
 			name: 'Książka',
 			price: '55',
+			_id: 8,
 		},
 		{
 			date: '05.03.2024',
 			name: 'Patelnia',
 			price: '66,34',
+			_id: 9,
+		},
+		{
+			date: '05.03.2024',
+			name: 'Netflix',
+			price: '34,99',
+			_id: 10,
+		},
+		{
+			date: '05.03.2024',
+			name: 'Fryzjer',
+			price: '37',
+			_id: 11,
 		},
 		{
 			date: '23.02.2024',
 			name: 'Buty',
 			price: '250',
+			_id: 12,
 		},
 		{
 			date: '11.02.2024',
 			name: 'Kurtka',
 			price: '180',
+			_id: 13,
 		},
 	]);
 	const settingsBoxRef = useRef(null);
@@ -74,8 +104,14 @@ function Transactions() {
 		settingsBoxRef.current.classList.toggle('settings-open');
 	};
 
-	const addModalHandler = () => {
-		setaddModalIsOpen(!addModalIsOpen);
+	const addModalHandler = (x) => {
+		if (x === 'addBtn') {
+			setTimeout(() => {
+				setaddModalIsOpen(!addModalIsOpen);
+			}, 50);
+		} else {
+			setaddModalIsOpen(!addModalIsOpen);
+		}
 	};
 
 	return (
@@ -86,37 +122,73 @@ function Transactions() {
 			<Navigation />
 			{/* TOP BAR */}
 			<div className='top-bar-app'>
-				<button
-					className='top-bar-app__settings-menu-btn'
-					type='button'
-					onClick={settingsMenuHandler}>
-					<span className='top-bar-app__settings-menu-icon'>
-						<FontAwesomeIcon icon={faEllipsisVertical} />
-					</span>
-					<div ref={settingsBoxRef} className='top-bar-app__settings-box'>
-						<button className='top-bar-app__settings-btn'>
-							<span className='top-bar-app__settings-icon'>
-								<FontAwesomeIcon icon={faGear} />
-							</span>
-							<span className='top-bar-app__settings-text'>Ustawienia</span>
-						</button>
-						<span className='top-bar-app__settings-line'></span>
-						<button className='top-bar-app__settings-btn'>
-							<span className='top-bar-app__settings-icon'>
-								<FontAwesomeIcon icon={faRightFromBracket} />
-							</span>
-							<span className='top-bar-app__settings-text'>Wyloguj</span>
-						</button>
+				<div className='top-bar-app__top-bar'>
+					<button
+						className='top-bar-app__settings-menu-btn'
+						type='button'
+						onClick={settingsMenuHandler}>
+						<span className='top-bar-app__settings-menu-icon'>
+							<FontAwesomeIcon icon={faEllipsisVertical} />
+						</span>
+						<div ref={settingsBoxRef} className='top-bar-app__settings-box'>
+							<button className='top-bar-app__settings-btn'>
+								<span className='top-bar-app__settings-icon'>
+									<FontAwesomeIcon icon={faGear} />
+								</span>
+								<span className='top-bar-app__settings-text'>Ustawienia</span>
+							</button>
+							<span className='top-bar-app__settings-line'></span>
+							<button className='top-bar-app__settings-btn'>
+								<span className='top-bar-app__settings-icon'>
+									<FontAwesomeIcon icon={faRightFromBracket} />
+								</span>
+								<span className='top-bar-app__settings-text'>Wyloguj</span>
+							</button>
+						</div>
+					</button>
+					<div className='top-bar-app__input-box'>
+						<input
+							className='top-bar-app__input'
+							type='text'
+							placeholder='Szukaj'></input>
+						<span className='top-bar-app__input-icon'>
+							<FontAwesomeIcon icon={faMagnifyingGlass} />
+						</span>
 					</div>
-				</button>
-				<div className='top-bar-app__input-box'>
-					<input
-						className='top-bar-app__input'
-						type='text'
-						placeholder='Szukaj'></input>
-					<span className='top-bar-app__input-icon'>
-						<FontAwesomeIcon icon={faMagnifyingGlass} />
-					</span>
+				</div>
+				<div className='top-bar-app__bottom-bar'>
+					<div className='top-bar-app__cost-income-box'>
+						<span className='top-bar-app__bottom-bar-icon top-bar-app__income-icon'>
+							<FontAwesomeIcon icon={faSquareUpRight} />
+						</span>
+						<span className='top-bar-app__income'>
+							2100
+							<span className='top-bar-app__income-currency top-bar-app__currency'>
+								zł
+							</span>
+						</span>
+
+						<span className='top-bar-app__bottom-bar-icon top-bar-app__cost-icon'>
+							<FontAwesomeIcon icon={faSquareUpRight} />
+						</span>
+						<span className='top-bar-app__cost'>
+							1500
+							<span className='top-bar-app__cost-currency top-bar-app__currency'>
+								zł
+							</span>
+						</span>
+					</div>
+					<div className='top-bar-app__balance-box'>
+						<span className='top-bar-app__balance-icon'>
+							<FontAwesomeIcon icon={faPlus} />
+						</span>
+						<span className='top-bar-app__balance'>
+							600
+							<span className='top-bar-app__balance-currency top-bar-app__currency'>
+								zł
+							</span>
+						</span>
+					</div>
 				</div>
 			</div>
 			{/* BOTTOM BUTTONS */}
@@ -150,24 +222,60 @@ function Transactions() {
 			<div className='transactions'>
 				{transactions.map((transaction) => {
 					let isDifferent;
+					let isFirst;
+					let isBetween;
+					let isLast;
 
 					const index = transactions.findIndex((el) => {
 						return el === transaction;
 					});
 
-					if (transactions[index]?.date !== transactions[index - 1]?.date) {
+					if (
+						transactions[index - 1]?.date !== transactions[index]?.date &&
+						transactions[index]?.date === transactions[index + 1]?.date
+					) {
 						isDifferent = true;
-					} else {
-						isDifferent = false;
+						isFirst = true;
+					} else if (
+						transactions[index - 1]?.date !== transactions[index]?.date
+					) {
+						isDifferent = true;
+					} else if (
+						transactions[index - 1]?.date === transactions[index + 1]?.date
+					) {
+						isBetween = true;
+					} else if (
+						transactions[index]?.date !== transactions[index + 1]?.date
+					) {
+						isLast = true;
 					}
 
+					// if (transactions[index]?.date !== transactions[index - 1]?.date) {
+					// 	isDifferent = true;
+					// }
+
+					// if (transactions[index]?.date === transactions[index + 1]?.date) {
+					// 	isFirst = true;
+					// }
+
+					// if (transactions[index - 1]?.date === transactions[index + 1]?.date) {
+					// 	isBetween = true;
+					// }
+
+					// if (transactions[index]?.date !== transactions[index + 1]?.date) {
+					// 	isLast = true;
+					// }
+
 					return (
-						<>
+						<React.Fragment key={transaction._id}>
 							{transaction === transactions[0] || isDifferent ? (
 								<>
 									<span className='transactions__date'>{transaction.date}</span>
 
-									<div className='transactions__transaction'>
+									<div
+										className={`transactions__transaction ${
+											isFirst ? 'transaction-first-border' : ''
+										}`}>
 										<span className='transactions__icon-bg'>
 											<span className='transactions__icon'>
 												<FontAwesomeIcon icon={faCartShopping} />
@@ -186,7 +294,10 @@ function Transactions() {
 									</div>
 								</>
 							) : (
-								<div className='transactions__transaction'>
+								<div
+									className={`transactions__transaction ${
+										isBetween ? 'transaction-between-border' : ''
+									} ${isLast ? 'transaction-last-border' : ''}`}>
 									<span className='transactions__icon-bg'>
 										<span className='transactions__icon'>
 											<FontAwesomeIcon icon={faCartShopping} />
@@ -204,10 +315,10 @@ function Transactions() {
 									</div>
 								</div>
 							)}
-						</>
+						</React.Fragment>
 					);
 				})}
-				<div className='transactions__container'>
+				{/* <div className='transactions__container'>
 					<span className='transactions__date'>12.07.2024</span>
 					<div className='transactions__transaction'>
 						<span className='transactions__icon-bg'>
@@ -225,7 +336,7 @@ function Transactions() {
 							</span>
 						</div>
 					</div>
-				</div>
+				</div> */}
 			</div>
 			<div className='page-bg'></div>
 		</>
